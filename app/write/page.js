62a225d8bd30957eb62d66e0c16a079cae4ddc08
jkatagata1228 <j.katagata1228@gmail.com>
2024-03-12 17:48:19 +0900

    fix/refactoring

--- app/write/page.js
+++ app/write/page.js
@@ -12,17 +12,6 @@ import WriteReactQuill from "../ReactQuill";
 //   //   로그인하세요
 //   // </div>}
 
-//   return (
-//     <div className="p-20">
-//       <h4>글작성</h4>
-//       <form action="/api/post/new" method="POST">
-//         <input name="title" placeholder="title"></input>
-//         <input name="content" placeholder="content"></input>
-//         <button type="submit">test</button>
-//       </form>
-//     </div>
-//   );
-// }
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 function Write() {
