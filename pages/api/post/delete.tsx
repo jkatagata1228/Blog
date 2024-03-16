2be6840ad626afd5ec9cfb448deb05b1ae4164ca
JANG JUNYONG <jangjunyong@JANGnoMacBook-Air.local>
2024-03-16 18:06:29 +0900

    fix/refactoring

--- pages/api/post/delete.tsx
+++ pages/api/post/delete.tsx
@@ -4,23 +4,28 @@ import { authOptions } from "../auth/[...nextauth]";
 import { connectDB } from "../../../util/database";
 
 export default async function handler(req, res) {
-  let a = await getServerSession(req, res, authOptions);
+  let session = await getServerSession(req, res, authOptions);
 
   const db = (await connectDB).db("forum");
-  let result = await db.collection("post").findOne({ _id: new ObjectId(req.body) });
-  console.log(result);
-  if (req.method == "DELETE") {
-    if (result.author == a.user.email) {
-      {
-        try {
-          const db = (await connectDB).db("forum");
-          await db.collection("post").deleteOne({ _id: new ObjectId(req.body) });
-          return res.redirect(302, "/list");
-        } catch (error) {
-          return res.status(500).json("sorry");
+  let result = await db
+    .collection("post")
+    .findOne({ _id: new ObjectId(req.body) });
+  if (session && session.user) {
+    if (req.method == "DELETE") {
+      if (result.author == session.user.email) {
+        {
+          try {
+            const db = (await connectDB).db("forum");
+            await db
+              .collection("post")
+              .deleteOne({ _id: new ObjectId(req.body) });
+            return res.redirect(302, "/list");
+          } catch (error) {
+            return res.status(500).json("sorry");
+          }
         }
-      }
-    } else console.log("x");
+      } else console.log("x");
+    }
   }
 
   // 서버기능 처리성공return res.status(200).json("ok");
