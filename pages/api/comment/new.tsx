ed5370d256c31f8e92bdc2275937835ab46948cf
JANG JUNYONG <jangjunyong@JANGnoMacBook-Air.local>
2024-03-16 17:59:28 +0900

    fix/refactoring

--- pages/api/comment/new.tsx
+++ pages/api/comment/new.tsx
@@ -4,21 +4,25 @@ import { authOptions } from "../auth/[...nextauth]";
 import { connectDB } from "../../../util/database";
 
 export default async function handler(req, res) {
-  let session = await getServerSession(req, res, authOptions);
+  const session = await getServerSession(req, res, authOptions);
   req.body = JSON.parse(req.body);
-  let save = {
-    content: req.body.comment,
-    parent: new ObjectId(req.body._id),
-    author: session.user.email,
-  };
-  if (req.method == "POST") {
-    try {
-      const db = (await connectDB).db("forum");
-      await db.collection("comment").insertOne(save);
-      //redirect안되고있음 수정필
-      return res.redirect(302, "/list");
-    } catch (error) {
-      return res.status(500).json("sorry");
+  if (session && session.user) {
+    const save = {
+      content: req.body.comment,
+      parent: new ObjectId(req.body._id),
+      author: session.user.email,
+    };
+    if (req.method == "POST") {
+      try {
+        const db = (await connectDB).db("forum");
+        await db.collection("comment").insertOne(save);
+        //redirect안되고있음 수정필
+        // return res.redirect(302, "/list");
+        res.writeHead(302, { Location: "/list" });
+        res.end();
+      } catch (error) {
+        return res.status(500).json("sorry");
+      }
     }
   }
 
