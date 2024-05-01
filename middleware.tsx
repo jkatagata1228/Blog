fb9db09a7575c08065ede2199b2fa895ed46f8dc
jkatagata1228 <124960251+jkatagata1228@users.noreply.github.com>
2024-05-01 18:49:08 +0900

    Update middleware.tsx

--- middleware.tsx
+++ middleware.tsx
@@ -5,7 +5,7 @@ export function middleware(req: NextRequest) {
   const developToken = req.cookies.get("next-auth.session-token");
   const releaseToken = req.cookies.get("__Secure-next-auth.session-token");
 
-  if (!developToken || !releaseToken) {
+  if (!developToken && !releaseToken) {
     return NextResponse.redirect(new URL("/list", req.url));
   }
 }
